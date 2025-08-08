a5c6e0d8386fbda629ee84b31c95748da8d3b0db
RSP-007 <217321612+RSP-007@users.noreply.github.com>
2025-08-08 05:37:41 -1200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+        //This is a comment not displayed
+        /*This is a multiline
+          comment okay
+         */
+        System.out.println("HELLO THIS IS MY FIRST JAVA PROGRAM");
+        System.out.println("THe world is moving fast but slow");
+        System.out.println("Trying to learn");
+    }
+}
+
+/*   OUTPUT:
+HELLO THIS IS MY FIRST JAVA PROGRAM
+THe world is moving fast but slow
+Trying to learn
+
+ */
